8bd34d41efdc36404a77c45927b83c47ac65f8f0
Bruno Leonardo Michels <brunolm7@gmail.com>
2018-06-17 18:50:05 -0300

    Add

--- projects/ngx-redux-state-props/src/lib/ngx-redux-state-props.module.ts
+++ projects/ngx-redux-state-props/src/lib/ngx-redux-state-props.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
+import { NgxReduxStatePropsService } from './ngx-redux-state-props.service';
+import { NgxReduxStatePropsComponent } from './ngx-redux-state-props.component';
 
 @NgModule({
   imports: [],
-  declarations: [],
-  exports: [],
+  declarations: [NgxReduxStatePropsComponent],
+  providers: [NgxReduxStatePropsService],
+  exports: [NgxReduxStatePropsComponent],
 })
 export class NgxReduxStatePropsModule {}
--- projects/ngx-redux-state-props/src/public_api.ts
+++ projects/ngx-redux-state-props/src/public_api.ts
@@ -3,4 +3,5 @@
  */
 
 export * from './lib/ngx-redux-state-props.service';
+export * from './lib/ngx-redux-state-props.component';
 export * from './lib/ngx-redux-state-props.module';
